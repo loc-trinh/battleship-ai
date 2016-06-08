227bf3ed73b41eb9e8741fccb557ed20f2ac3213
Loc Trinh <loctrinh@mit.edu>
2016-06-08 06:28:20 -0400

    now done with board.py

--- board.py
+++ board.py
@@ -7,16 +7,30 @@ import random
 
 class Board:
     def __init__(self):
-        ''' Initialize random board. Using the following pieces:
-            Scout(2), Submarine(3), Cruiser(3), Battleship(4), Carrier(5)
-        '''
-
-        self.board = ['-'] * 100
+        self.board = []
         self.ships = [2,3,3,4,5] 
         self.ship_locations = set()
-        
+        self.generate_board()
+       
+
+    def __str__(self):
+        """ Return printed board in grid fashion """ 
+
+        board_string = "  A B C D E F G H I J"
+        for i in range(0, 100, 10):
+            board_string += "\n" + str(i/10) + " " + " ".join(self.board[i:i+10]) 
+
+        return board_string
+
+
+    def generate_board(self):
+        """ Initialize random board. Using the following pieces:
+            Scout(2), Submarine(3), Cruiser(3), Battleship(4), Carrier(5)
+        """
+
+        self.board = ['-'] * 100                #reset board
         while True:
-            locations = []
+            ship_locations = []
             for ship in self.ships:
                 # pick a row or column
                 if random.randint(0,1):
@@ -28,25 +42,18 @@ class Board:
 
                 # randomly draw a ship
                 index = random.randrange(10-ship)
-                locations += indicies[index:index+ship]
+                ship_locations += indicies[index:index+ship]
 
-            if len(set(locations)) == 17:
-                self.ship_locations = set(locations)
+            if len(set(ship_locations)) == 17:
+                self.ship_locations = set(ship_locations)
                 for i in self.ship_locations:
                     self.board[i] = 'o'
                 break
 
-    def __str__(self):
-        ''' Return printed board in grid fashion '''
-
-        board_string = "  A B C D E F G H I J"
-        for i in range(0, 100, 10):
-            board_string += "\n" + str(i/10) + " " + " ".join(self.board[i:i+10]) 
-
-        return board_string
-
-
     def play(self, move):
+        """ Play a move. Return "HIT" or "MISS" if there's a ship at location
+        """
+
         index = self.convert_move_to_index(move)
         if index == -1:
             return "Invalid move!"
@@ -57,6 +64,8 @@ class Board:
 
 
     def convert_move_to_index(self, move):
+        """ Helper functions to convert moves into board index """
+
         if len(move) != 2:
             return -1
         column = move[0]
